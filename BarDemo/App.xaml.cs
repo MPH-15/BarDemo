3475ca521e7891a48c32d31140d047e293d700be
Matt Hinojosa <mhinojosa0022@hotmail.com>
2019-03-04 18:00:45 -0600

    Initial commit

---
+++ BarDemo/App.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using BarDemo.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace BarDemo
+{
+    public partial class App : Application
+    {
+        public static bool IsUserLoggedIn { get; set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            if (!IsUserLoggedIn)
+            {
+                MainPage = new NavigationPage(new LoginPage());
+            }
+            else
+            {
+                MainPage = new NavigationPage(new TabPage());
+            }
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
